63c25e6be3b468178d73123e8683ac532db3a83e
DiepThu <sdrankmasters@gmail.com>
2024-06-21 01:44:18 +0900

    .

---
+++ BusinessLogicLayer/Viewmodels/ViewKH/ProDetailKH.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Viewmodels.ViewKH
+{
+    public class ProDetailKH
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string Size { get; set; }
+        public string Color { get; set; }
+        public string UrlImg { get; set; }
+    }
+}
